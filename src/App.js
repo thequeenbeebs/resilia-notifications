51fed5e8c8b84edca7a7d89aace062fc419bf18a
Blaire Baker <bakerblaire@gmail.com>
2021-08-04 17:43:40 -0600

    build out notifications component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Notification from './Notification'
 
 function App() {
   const [notifications, setNotifications] = useState([])
@@ -10,8 +11,8 @@ function App() {
   })
   return (
     <div>
-      Resilia Notifications
-      {notifications.map(notification => <p>{notification.message}</p>)}
+      <h1>Resilia Notifications</h1>
+      {notifications.map(notification => <Notification message={notification.message}/>)}
     </div>
   );
 }
---
+++ src/Notification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Notification (props ) {
+    return (
+        <div>
+            <p>{props.message}</p>
+            <button>CLOSE</button>
+        </div>
+    )
+}
+
+export default Notification;
