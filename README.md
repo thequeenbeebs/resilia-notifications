aa6300e3a8b88d10ea5aaff27f9233a67ceca202
Blaire Baker <bakerblaire@gmail.com>
2021-08-05 12:55:07 -0600

    notification basic styling

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-Current Goals:
-* Implement local storage
-
 Future Goals:
 * Enter username to see notifications
 * Using Authentication and a login rather than just hard coding a user
+* Styling
--- src/Notification.js
+++ src/Notification.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Notification ( { notification, deleteNotification} ) {
     return (
-        <div>
+        <div className="notification">
             <p>{notification.message}</p>
             <button onClick={() => deleteNotification(notification)}>CLOSE</button>
         </div>
